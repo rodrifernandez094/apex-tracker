d3f9222bd859b8e73a411ed34a10e0224ef869f4
rodrifernandez094 <rodrifernandez094@gmail.com>
2023-01-09 15:16:26 -0300

    Add catch all route

--- server.js
+++ server.js
@@ -9,11 +9,23 @@ const app = express();
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/profile", routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client/dist")));
 }
+
+app.use("/api/profile", routes);
+
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/dist/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`server running on port ${port}`));
